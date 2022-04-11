5c11399bb3922403f691f1d37f6c48f8d319d47a
Marius <marius@example.com>
2022-04-11 09:14:14 +0300

    init commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Node 4
+
+1. sukuriam src/server.js
+2. prisidedam git, github
+3. inicijuojam npm
+4. instaliuojam express, cors, morgan
+5. pakuriam serveri su port 3000
+6. parassom GET / - grazina h1 'hello express'
---
+++ src/db/db.js
@@ -0,0 +1,32 @@
+const numbers = [1, 15, 9, -9, -8, -7, 2, 8, 51, 6, 5];
+
+const posts = [
+  {
+    id: 1,
+    title: 'Post 1',
+    body: 'This is Post 1 body and it is all about Post 1',
+  },
+  {
+    id: 2,
+    title: 'Post 2',
+    body: 'This is Post 2 body and it is all about Post 2',
+  },
+  {
+    id: 3,
+    title: 'Post 3',
+    body: 'This is Post 3 body and it is all about Post 3',
+  },
+  {
+    id: 4,
+    title: 'ExpressJs',
+    body: 'Express is easy way to make back end',
+  },
+];
+
+function getPositiveNumbers() {}
+
+function getNumbersGreaterThan(x) {}
+
+function getPostById(id) {}
+
+function getPostByTitle(title) {}
