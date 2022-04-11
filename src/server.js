673f89d98f98b1b96152e7dbf87987c7b472fae0
Marius <marius@example.com>
2022-04-11 11:08:24 +0300

    dynam uzd

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(morgan('dev'));
 
 app.get('/', (request, response) => {
   response.send('<h1> Hello Express </h1>');
@@ -26,6 +32,8 @@ app.get('/numbers/positives', (request, response) => {
 
 // GET /numbers/gt/10 - grazina masyva kuriame yra sk didesni uz 10
 
+// GET /numbers/gt/:num - grazina masyva kuriame yra sk didesni uz num
+
 // GET /posts - grazina posts masyva json formatu is db.js
 const { posts } = require('./db/db');
 app.get('/posts', (request, response) => {
@@ -46,6 +54,11 @@ app.get('/posts/:postId', (req, res) => {
   const postId = +req.params.postId;
   console.log('postId ===', postId);
   const foundPost = posts.find((pObj) => pObj.id === postId);
+  console.log('foundPost ===', foundPost);
+  if (foundPost === undefined) {
+    res.status(404).json({ error: 'Post not found' });
+    return;
+  }
   res.json(foundPost);
 });
 
